68e28bf7e816f680cf3e8c8a3a7d5cbc583f4ab6
TMsudarsan <sudarsantm10@gmail.com>
2025-06-08 23:43:49 +0530

    commit1

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,9 +23,9 @@ app.use(cors({
 app.use("/api/athu", authRoute);
 app.use("/api/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
  
