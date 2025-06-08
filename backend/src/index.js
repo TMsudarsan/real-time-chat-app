4f5346c880797763197c7bc4bc77d05ca2e9278c
TMsudarsan <sudarsantm10@gmail.com>
2025-06-08 23:44:14 +0530

    commit2

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,8 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+
+;
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
@@ -27,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+} 
  
 server.listen(PORT,()=>{
     console.log("server is run, in the port number " + PORT);
