ddb28027003437b2bb621ab3ebd324c6a76f89c4
TMsudarsan <sudarsantm10@gmail.com>
2025-06-09 09:14:55 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-;
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
@@ -23,6 +22,7 @@ app.use(cors({
 }))
 
 app.use("/api/athu", authRoute);
+
 app.use("/api/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
