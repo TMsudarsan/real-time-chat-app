25066273be83ba925835fd62bfc921e94dad5fc7
TMsudarsan <sudarsantm10@gmail.com>
2025-06-09 09:51:23 +0530

    Corrected folder name to frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
@@ -22,7 +23,6 @@ app.use(cors({
 }))
 
 app.use("/api/athu", authRoute);
-
 app.use("/api/messages", messageRouter);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
